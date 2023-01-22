84806656505e89842179d7f3fd5391ba949a832f
yukuda <leeyookoo@naver.com>
2023-01-22 22:43:26 +0900

    유효성 검사

--- src/main/java/com/study/joiner/web/dto/UserResponseDto.java
+++ src/main/java/com/study/joiner/web/dto/UserResponseDto.java
@@ -1,6 +1,9 @@
 package com.study.joiner.web.dto;
 
 import com.study.joiner.domain.user.SocialUser;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -9,11 +12,23 @@ import java.time.format.DateTimeFormatter;
 @Getter
 public class UserResponseDto {
     private Long id;
+
+    @NotBlank(message = "아이디는 필수 입력값입니다.")
+    @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "아이디는 영어 소문자와 숫자만 사용하여 4~20자리여야 합니다.")
     private String name;
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email
     private String email;
+
     private String picture;
+
+    @NotBlank(message = "닉네임은 필수 입력값입니다.")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,10}$" , message = "닉네임은 특수문자를 포함하지 않은 2~10자리여야 합니다.")
     private String nickName;
+
     private String content;
+
     private LocalDateTime createdDate;
     private String date;
 
