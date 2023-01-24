a747f553076d2f366349d3c3cb106e698939bb4a
Administrator <leedero01@naver.com>
2023-01-25 04:44:20 +0900

    [CHORE] Update BoardRepository

--- src/main/java/com/study/joiner/repository/BoardRepository.java
+++ src/main/java/com/study/joiner/repository/BoardRepository.java
@@ -4,15 +4,18 @@ package com.study.joiner.repository;
 import com.study.joiner.domain.user.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    @Query("select b from Board b order by b.createdDate desc")
-    List<Board> findAllDesc();
+
+    @EntityGraph(attributePaths = {"socialUser", "commentList.user"})
+    Optional<Board> findById(Long id);
 
     @Query("select b from Board b order by b.createdDate desc")
     Page<Board> findAllDesc(Pageable pageable);
